315b0f59dceaf6947fcdea579b909ea10b87254a
chenlong <chenlongim@gmail.com>
2017-04-01 17:15:16 +0800

    1. 添加checkbox记住用户名与密码 2. 采用SharedPreference存储

--- app/src/main/java/com/aaron/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/aaron/broadcastbestpractice/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.aaron.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,13 +19,31 @@ public class LoginActivity extends BaseActivity {
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+    private CheckBox rememberPass;
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember = pref.getBoolean("remeber_password", false);
+        if (isRemember) {
+            //将账号和密码都设置到文本框中
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +51,17 @@ public class LoginActivity extends BaseActivity {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
 
-                if ("admin".equals(account) && "123456".equals(password)) {
+                if ("Aaron".equals(account) && "123456".equals(password)) {
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()) {
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.commit();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
